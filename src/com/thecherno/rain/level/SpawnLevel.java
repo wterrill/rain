023709b85035e3b3226c1859da21169ede1c0501
Will Terrill <william.terrill@gmail.com>
2015-12-27 22:46:09 -0600

    added Spawnlevel.java class per episode 52

---
+++ src/com/thecherno/rain/level/SpawnLevel.java
@@ -0,0 +1,13 @@
+package com.thecherno.rain.level;
+
+public class SpawnLevel extends Level {
+
+	
+
+	public SpawnLevel(String path) {
+		super(path);
+	}
+	protected void loadLevel(String path) {
+
+	}
+}
