5904f8ae158bfa37c2c95002d466e98bba27cb1b
Will Terrill <william.terrill@gmail.com>
2016-01-08 16:47:28 -0600

    started manipulating the physics of the particles

--- src/com/thecherno/rain/entity/particle/Particle.java
+++ src/com/thecherno/rain/entity/particle/Particle.java
@@ -9,15 +9,16 @@ public class Particle extends Entity {
 	private int life;
 	private int time;
 	
-	protected double xx,yy,xa, ya;
+	protected double xx,yy, zz;
+	protected double xa, ya, za;
 	
 	public Particle(int x, int y, int life){
-		System.out.println("particle life " + life);
 		this.x = x;
 		this.y = y;
 		this.xx = x;
 		this.yy = y;
-		this.xa = random.nextGaussian();
+		this.zz = 20.0;
+		this.xa = random.nextGaussian() + 2.0;
 		this.ya = random.nextGaussian();
 		this.life = life;		
 		sprite = Sprite.paricle_normal;
@@ -27,11 +28,19 @@ public class Particle extends Entity {
 		time++;
 		if (time >= 8000) time = 0;
 		if(time > life) remove();
+		za -= 0.1;
+		if(zz<0){ //zz = 0 is our 'floor
+			zz=0; //we keep the z value pinned to the floor.
+			za*=-0.8; //we multiply by -0.8 in order to have the velocity change direction and get reduced for every 'bounce'
+			xa*=0.4; //to keep particles grouped, we half the x and y distances.
+			ya*=0.4;
+		}
 		this.xx += xa;
 		this.yy += ya;
+		this.zz += za;
 	}
 	
 	public void render (Screen screen){
-		screen.renderSprite((int)xx,(int)yy,sprite,true);
+		screen.renderSprite((int)xx,(int)yy - (int)zz,sprite,true);
 	}
 }
