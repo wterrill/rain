49228451bb3d20c8641e944741478798a7f26c56
wterrill <william.terrill@gmail.com>
2016-01-12 16:17:43 -0600

    Better projectile bouncing.  I left this one with a long life and elastic collisions.  It kind of makes me laugh.

--- src/com/thecherno/rain/entity/Projectile/WizardProjectile.java
+++ src/com/thecherno/rain/entity/Projectile/WizardProjectile.java
@@ -13,17 +13,17 @@ public class WizardProjectile extends Projectile {
 
 	public WizardProjectile(int x, int y, double dir) {
 		super(x, y, dir);
-		range = random.nextInt(50)+80;
+		range = random.nextInt(20) + 300;
 		damage = 20;
-		speed = 3;
+		speed = 0.55;
 		sprite = Sprite.projectile_wizard;
 		nx = speed * Math.cos(dir);
 		ny = speed * Math.sin(dir);		
 	}
 
 	public void update(){
-		if (level.tileCollision((int)(x + nx), (int)(y + ny), 7,5,4)){  //5 and 4 are the number of pixels from the upper left corner to the plane of the pojectile in the x and y direction. (max extension)
-			int life = 20;
+		if (level.tileCollision((int)(x + nx), (int)(y + ny), 7, 5, 4)){  //5 and 4 are the number of pixels from the upper left corner to the plane of the pojectile in the x and y direction. (max extension)
+			int life = 200;
 			int amount = 10;
 			level.add(new ParticleSpawner((int)x, (int)y, life, amount, level));
 			remove();
@@ -44,6 +44,6 @@ public class WizardProjectile extends Projectile {
 		return dist;
 	}
 	public void render(Screen screen){
-		screen.renderProjectile((int)x -8, (int)y -8, this);
+		screen.renderProjectile((int)x - 8, (int)y - 8, this);
 	}
 }
--- src/com/thecherno/rain/entity/particle/Particle.java
+++ src/com/thecherno/rain/entity/particle/Particle.java
@@ -17,11 +17,10 @@ public class Particle extends Entity {
 		this.y = y;
 		this.xx = x;
 		this.yy = y;
-		this.zz = 20;
+		this.zz = random.nextFloat() + 2.0;
 		this.xa = random.nextGaussian();
-		if (this.xa <0) this.xa = 0.1;
 		this.ya = random.nextGaussian();
-		this.life = life;		
+		this.life = life + random.nextInt(400) - 10;		
 		sprite = Sprite.paricle_normal;
 	}
 
@@ -32,16 +31,43 @@ public class Particle extends Entity {
 		za -= 0.1;
 		if(zz<0){ //zz = 0 is our 'floor
 			zz=0; //we keep the z value pinned to the floor.
-			za*=-0.8; //we multiply by -0.8 in order to have the velocity change direction and get reduced for every 'bounce'
-			xa*=0.4; //to keep particles grouped, we half the x and y distances.
-			ya*=0.4;
+			za*=-0.6; //we multiply by -0.8 in order to have the velocity change direction and get reduced for every 'bounce'
+			//xa*=0.4; //to keep particles grouped, we half the x and y distances.
+			//ya*=0.4;
+		}
+		move((xx + xa), (yy + ya) + (zz + za));
+	}
+	
+	private void move(double x, double y) {
+		if (collision(x,y)) {
+			this.xa *= -0.5;
+			this.ya *= -0.5;
+			this.za *= -0.5;
 		}
 		this.xx += xa;
 		this.yy += ya;
 		this.zz += za;
+		
 	}
-	
+
+	public boolean collision (double x, double y){
+		boolean solid = false;
+		for(int c = 0; c < 4; c++){  //These are here to "look" in each of the four different directions. 
+			double xt = (x - c % 2 * 16) /16.0;				
+			double yt = (y - c/2 * 16 )  /16.0;
+			int ix = (int) Math.ceil(xt);
+			int iy = (int) Math.ceil(yt);
+			if (c % 2 == 0){
+				ix = (int)Math.floor(xt);
+			}//c = 0 and 2 =  RIGHT.
+			
+			if (c / 2 == 0) iy = (int) Math.floor(yt); //c = 0 = DOWN
+			
+			if (level.getTile(ix,iy).solid()) solid = true;
+		}
+		return solid;
+	}
 	public void render (Screen screen){
-		screen.renderSprite((int)xx + 2,(int)yy - (int)zz + 20,sprite,true);
+		screen.renderSprite((int)xx ,(int)yy - (int)zz - 3,sprite,true);
 	}
 }
